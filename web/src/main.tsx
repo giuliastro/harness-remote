32a19c850df4c176fbde211b6abdf8cb9ca56edf
giuliastro <giuliaccio@gmail.com>
2026-08-21 08:55:23 +0200

    style(taskdesk): load audited mobile surfaces last

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -34,6 +34,7 @@ import "./taskdesk-v3-unified.css"
 import "./taskdesk-run-review.css"
 import "./v3-polish.css"
 import "./taskdesk-mobile-ux.css"
+import "./taskdesk-mobile-surfaces.css"
 
 installAppPreferences()
 installCompletionAudioGuard()
